bba6484c03398e1b9fcd64e4c4195a6cc56b1936
rahulyadavhub <rahulyadav.prof@gmail.com>
2026-06-13 01:01:20 +0530

    fix: correct BASE_URL to include /api/ prefix for admin endpoints

--- app/src/main/java/com/trybild/attendr/utils/Constants.kt
+++ app/src/main/java/com/trybild/attendr/utils/Constants.kt
@@ -1,7 +1,7 @@
 package com.trybild.attendr.utils
 
 object Constants {
-    const val BASE_URL = "https://attendr-mfyr.onrender.com/"
+    const val BASE_URL = "https://attendr-mfyr.onrender.com/api/"
     const val TOKEN_KEY = "jwt_token"
     const val DEVICE_ID_KEY = "device_id"
     const val USER_KIND_KEY = "user_kind"
